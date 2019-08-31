b088949ac05c17dc5153b8a082f8bd5b1257a631
realingy <754037927@qq.com>
2019-08-31 11:21:00 +0800

    图像的预处理

---
+++ 2_PreProcess/clahe/clahe.py
@@ -0,0 +1,21 @@
+import cv2
+import matplotlib.pyplot as plt
+
+img = cv2.imread('timg.jpg',0) #直接读为灰度图像
+res = cv2.equalizeHist(img)
+
+clahe = cv2.createCLAHE(clipLimit=2,tileGridSize=(10,10))
+cl1 = clahe.apply(img)
+
+plt.subplot(131),plt.imshow(img,'gray')
+plt.subplot(132),plt.imshow(res,'gray')
+plt.subplot(133),plt.imshow(cl1,'gray')
+
+plt.show()
+
+cv2.imshow("src", img)
+cv2.imshow("equalizeHist", res)
+cv2.imshow("CLAHE", cl1)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
